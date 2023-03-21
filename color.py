a46fafca476b1c28055198a83c5908d9a0cb6a41
rowonn <83148939+rowonn@users.noreply.github.com>
2023-03-21 16:15:28 +0800

    Create color.py
    color

---
+++ color.py
@@ -0,0 +1,6 @@
+color = (
+            'xkcd:blue','xkcd:orange','xkcd:green','xkcd:burple','tab:brown',\
+            'xkcd:hot pink','tab:gray','xkcd:olive','tab:cyan','xkcd:teal',\
+            'xkcd:electric blue','xkcd:orange red','xkcd:jade','xkcd:neon purple',\
+            'xkcd:terracotta','xkcd:pink', 'xkcd:grey blue', 'xkcd:light red','xkcd:night blue'
+            )
